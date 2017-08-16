1773bc485e212677a70b5d71f06625a4b2867bf8
Maren Wehrheim <marenwehrheim@hotmail.de>
2017-08-16 15:01:43 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = Restaurant.create([
+  {
+    name: "Italian Bistro",
+    address: "Viale Kramer 18, Milano",
+    phone_number: 00344534520445,
+    category: "italian"
+  },
+  {
+    name: "Maison ruge",
+    address: "Rue de Champs, 5",
+    phone_number: 00337364710235,
+    category: "french"
+  },
+  {
+    name: "Burger Palace",
+    address: "Highway 27",
+    phone_number: 0017465720432,
+    category: "american"
+  },
+  {
+    name: "Dorfstube",
+    address: "Rosenthalerplatz 23",
+    phone_number: 00474626304626,
+    category: "german"
+  },
+])
